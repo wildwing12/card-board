c55ba1a0f29cd7a710d66f0a7f388e628f0d159a
gaheul <rkgmf1230@naver.com>
2022-10-24 08:47:03 +0900

    2022.10.24

---
+++ src/Layout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
